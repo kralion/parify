09460887c5908f2e1e0fa01b0e3c0a3cf27f9dd1
Brayan Joan Paucar Marca <joan@Brayans-MacBook-Pro.local>
2023-08-16 18:23:52 -0700

    First Commit

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div className="text-red-200">About</div>;
+}
+
+export default About;
---
+++ components/button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button() {
+  return <div className="text-green-400">Button</div>;
+}
+
+export default Button;
