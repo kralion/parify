e620371819bd51b24c43f9da28c63b2c1fef41de
Joan <joan300501@gmail.com>
2023-09-11 13:42:19 -0700

    Project icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/pages/navbar";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter, Roboto_Condensed } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 const robotoCondensed = Roboto_Condensed({
@@ -13,6 +14,7 @@ const robotoCondensed = Roboto_Condensed({
 export const metadata: Metadata = {
   title: "Partify | Outsourcing your party planning needs.",
   description: "Outsourcing your party planning needs.",
+  creator: "Brayan Paucar",
 };
 
 export default function RootLayout({
@@ -22,6 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="https://cdn-icons-png.flaticon.com/128/1722/1722082.png"
+      />
       <body className={robotoCondensed.className}>
         <div className="">
           <NavBar />
