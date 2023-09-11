d14df79ddf1ac2d11db0b356c55e3fd66ad61cdf
Joan <joan300501@gmail.com>
2023-09-11 10:47:42 -0700

    First Github Action

--- app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About() {
-  return <div className="text-red-200">About</div>;
-}
-
-export default About;
--- components/pages/navbar.tsx
+++ components/pages/navbar.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Button } from "../ui/button";
 import { Dot } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const navLinks = [
-  { href: "/home", label: "Home" },
+  { href: "/", label: "Home" },
   { href: "/how-it-works", label: "How it Works" },
   { href: "/capabilities", label: "Capabilities" },
   { href: "/resource-hub", label: "Resource Hub" },
@@ -13,16 +14,18 @@ const navLinks = [
 export default function NavBar() {
   return (
     <div className="flex justify-between m-7 ">
-      <h2 className="text-4xl  flex items-center font-navaMono">
-        <Image
-          src="https://img.freepik.com/premium-vector/elegant-letter-sp-ps-minimalist-logo_649646-229.jpg?size=626&ext=jpg"
-          className="object-contain rounded-lg"
-          width={50}
-          height={50}
-          alt="Logo"
-        />
-        ARIFY
-      </h2>
+      <Link href="/">
+        <h2 className="text-4xl  flex items-center font-navaMono">
+          <Image
+            src="https://img.freepik.com/premium-vector/elegant-letter-sp-ps-minimalist-logo_649646-229.jpg?size=626&ext=jpg"
+            className="object-contain rounded-lg"
+            width={50}
+            height={50}
+            alt="Logo"
+          />
+          ARIFY
+        </h2>
+      </Link>
       <div className="flex gap-16">
         <div className="flex gap-3.5">
           {navLinks.map((link) => {
